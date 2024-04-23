0b8e850fc57e0222d32b095ca324c5593f6db381
Guilherme147828 <guilhermesantos99129@gmail.com>
2024-04-23 16:37:29 -0300

    criação variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,12 @@
+nome="Guilherme Santos"
+idade=16
+email='guilhermesantos99129@gmail.com'
+telefone='17 991398967'
+nota=8.99
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
