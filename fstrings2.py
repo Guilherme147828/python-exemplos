4b8a7990d4d74eb10bd81bda1e8d6d2429ffe7ea
Guilherme147828 <guilhermesantos99129@gmail.com>
2024-05-02 16:32:56 -0300

    Criação fstrings2.py

---
+++ fstrings2.py
@@ -0,0 +1,7 @@
+#Fortmatação de Strings com várias linhas
+escola = 'Senai'
+curso = 'Desenvolvimento de Sistemas'
+uc = 'Lógica de Programação'
+print(f"Escola: {escola}\n"
+      f"Curso: {curso}\n"
+      f"Unidade Curricular: {uc}")
